eeca645d0d3b9507f87923df6bddd3587abff6aa
Corrado Maurini <cmaurini@gmail.com>
2017-09-01 09:05:11 +0200

    update for lmm

--- publish.sh
+++ publish.sh
@@ -1,5 +1,7 @@
-git branch -D gh-pages
-git checkout -b gh-pages
+bundle exec jekyll build
+rsync -avzr _site/ corrado@bipbip.lmm.jussieu.fr:~/public_html/_site
+git branch -D master
+git checkout -b master
 git filter-branch --subdirectory-filter _site/ -f
 git checkout source
 git push --all --force origin
