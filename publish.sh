dda8f835117a27570637bea0589503578794f8c4
Corrado Maurini <cmaurini@gmail.com>
2018-08-19 11:25:33 +0200

    automatic workflow

--- publish.sh
+++ publish.sh
@@ -1,6 +1,9 @@
 bundle exec jekyll build
+# set up a ssh tunnel trhough chagall and rsync with bipbip
+ssh -fN -l corrado -L 2222:bipbip.lmm.jussieu.fr:22 chagall.dalembert.upmc.fr
 rsync -avzr ../articles/ corrado@bipbip.lmm.jussieu.fr:~/public_html/articles
 rsync -avzr _site/ corrado@bipbip.lmm.jussieu.fr:~/public_html/_site
+# sync with git on github
 git checkout source
 git add *
 git commit -m 'automatic workflow'
